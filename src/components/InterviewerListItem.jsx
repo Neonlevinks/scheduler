28e65b11ed47ec0eb8e424641299b5b52232090d
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-03-12 16:32:58 -0500

    Create Interviewer List component

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -2,22 +2,24 @@ import React from "react";
 
 import "components/InterviewerListItem.scss"
 
+import classNames from "classnames";
+
 export default function InterviewerListItem(props) {
 
-  const interviewer = {
-    key: props.id,
-    name: props.name,
-    avatar: props.avatar
-  }
+  let interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected" : props.selected
+  })
+
 
   return (
-    <li onClick={() => {props.setInterviewer(props.id)}}>
+    <li className={interviewerClass}
+      onClick={() => {props.setInterviewer(props.id)}}>
       <img
-      className="interviewers__item-image" 
+      className="interviewers__item-image"
       src={props.avatar}
       alt={props.name}
       />
-      {props.name}
+      {props.selected && props.name}
     </li>
   )
 }
