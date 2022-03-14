b3f7338e305cb9c4019a7844a2d249216343b98d
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-03-14 09:56:01 -0400

    Update cancel button to clear form and call props.onCancel

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -6,7 +6,17 @@ import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
-  const [interviewer, setInterviewer] = useState(props.interviewer || null)
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    return setStudent("") &&
+    setInterviewer(null)
+  };
+
+  const cancel = () => {
+    return reset() &&
+    props.onCancel
+  }
 
 
   return (
@@ -29,7 +39,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave} >Save</Button>
         </section>
       </section>
