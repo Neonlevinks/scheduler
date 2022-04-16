240f8c4c0a7a7d84a16c1270f7974219bc82853c
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2022-04-16 17:54:21 -0400

    Use axios to fetch data from api/days instead of hardcode

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,30 +1,91 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect  } from "react";
+import axios from "axios";
 import "components/Application.scss";
+
+import Appointment from "./Appointment/index.jsx";
 
 import DayList from "./DayList";
 
-const days = [
-  {
+
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
+
+const appointments = {
+  "1": {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
-  {
+  "2": {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
-  {
+  "3": {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
-];
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
 
+  const [ days, setDays ] = useState([]);
+
   const [ day, setDay] = useState("Monday");
+
+  const appointmentMapped = Object.values(appointments).map((appointment) => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    )
+  })
+
+  useEffect(() => {
+    const dayURL = `http://localhost:8001/api/days`;
+    axios.get(dayURL)
+    .then((response) => {
+      setDays([...response.data])
+    })
+  }, [])
 
   return (
     <main className="layout">
@@ -49,7 +110,8 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentMapped}
+        <Appointment time="5pm" />
       </section>
     </main>
   );
--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -3,14 +3,17 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
+  console.log(props);
+
   const dayData = props.days.map((day) => {
+
     return (
       <DayListItem
          key = {day.id}
          name = {day.name}
          spots = {day.spots}
          selected = {day.name === props.value}
-         setDay = {() => props.setDay(props.name)}
+         setDay = {() => props.onChange(day.name)}
       />
     )
   })
